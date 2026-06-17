07122a9916093c3dd94d0aa2e470b8ed96118104
Matheus-Merlos <matheusazrmerlos@gmail.com>
2026-06-16 22:23:55 -0300

    feat: extração de dados (teste)

---
+++ src/config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config:
+    _instance: Config | None  = None
+    _initialized: bool = False
+
+    def __new__(cls) -> "Config":
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        
+        return cls._instance
+
+    def __init__(self) -> None:
+        if not hasattr(self, '_initialized'):
+            self._initialized = True
+
+        self.prometheus_url: str | None = os.getenv('PROMETHEUS_CONNECTION_STRING')
+        prometheus_secure_connection_raw: str | None = os.getenv('PROMETHEUS_SECURE_CONNECTION')
+
+        if not self.prometheus_url:
+            raise ValueError('PROMETHEUS_CONNECTION_STRING value is invalid.')
+        
+        self.prometheus_secure_connection: bool = True if prometheus_secure_connection_raw == "true" else False
+        
---
+++ src/prometheus-extract.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from pandas import DataFrame
+from prometheus_api_client import PrometheusConnect, MetricRangeDataFrame
+from prometheus_api_client.utils import parse_datetime
+from datetime import datetime, timedelta
+from .config import Config
+
+def search_timeseries(prometheus_connection: PrometheusConnect, query: str, start_time: datetime, end_time: datetime, step: str):
+    result = prometheus_connection.custom_query_range(
+        query=query,
+        start_time=start_time,
+        end_time=end_time,
+        step=step
+    )
+
+    if not result:
+        return DataFrame(columns=['timestamp', 'value'])
+    
+    df_metric = MetricRangeDataFrame(result)
+    df_metric = df_metric.reset_index()
+    df_metric = df_metric[['timestamp', 'value']]
+    df_metric['value'] = pd.to_numeric(df_metric['value'])
+
+    df_metric['timestamp'] = df_metric['timestamp'].dt.floor('S')
+
+    return df_metric
+
+def extract_dataset():
+    config = Config()
+    prometheus_connection = PrometheusConnect(url=config.prometheus_url, disable_ssl=config.prometheus_secure_connection)
+
+    end_time: datetime | None = parse_datetime('now')
+    start_time = end_time - timedelta(days=30)
+    step = "1m"
+
+    namespace="production"
+    app_name="production-green-lms"
+
+    queries = {
+        'cpu_usage': f'sum(rate(container_cpu_usage_seconds_total{{namespace="{namespace}", pod=~"{app_name}.*"}}[5m]))', 
+        'mem_usage': f'sum(container_memory_working_set_bytes{{namespace="{namespace}", pod=~"{app_name}.*"}})',
+        'rps': f'sum(rate(nginx_ingress_controller_requests{{namespace="{namespace}", service=~".*{app_name}.*"}}[5m]))',
+        'replicas': f'kube_deployment_spec_replicas{{namespace="{namespace}", deployment=".*{app_name}"}}'
+    }
+
+    dataframes: list[DataFrame] = []
+
+    for column_name, query in queries.items():
+        df = search_timeseries(prometheus_connection, query, start_time, end_time, step)
+
+        df = df.rename(columns={'value': column_name})
+        dataframes.append(df)
+
+    final_df = dataframes[0]
+    for df in dataframes[1:]:
+        final_df = pd.merge(final_df, df, on='timestamp', how='outer')
+
+    final_df = final_df.sort_values('timestamp').reset_index(drop=True)
+    final_df = final_df.ffill()
+    final_df = final_df.dropna()
